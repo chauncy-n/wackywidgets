66c83dc7755b47bf279d82744018a1386f69471b
chauncy-n <eldoon@comcast.net>
2018-10-19 17:33:36 -0700

    it should be done

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='base'),
+    path('<int:pk>/delete', views.WidgetDelete.as_view(), name="widgets_delete"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Widget
+from django.http import HttpResponseRedirect
+from .forms import WidgetForm
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    if request.method == 'POST':
+        form = WidgetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        widgets = Widget.objects.all()
+        form = WidgetForm()
+        return render(request, 'base.html', {'widgets': widgets, 'form':form})
+
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = '__all__'
+
+class WidgetDelete(DeleteView):
+    model = Widget
+    success_url = '/'
+
+
+
